4bece01429145f2ba283d5e2db8999d98d8c5ce0
macgalaviz <macgalaviz@hotmail.com>
2020-08-02 19:49:18 -0600

    🚛 Rename App
    // Miguel Cabañas

--- android/app/src/main/java/com/reactnativewr/MainActivity.java
+++ android/app/src/main/java/com/reactnativewr/MainActivity.java
@@ -1,4 +1,4 @@
-package com.saspsp;
+package com.reactnativewr;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "saspsp";
+    return "reactnativewr";
   }
 }
--- ios/reactnativewrTests/reactnativewrTests.m
+++ ios/reactnativewrTests/reactnativewrTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface saspspTests : XCTestCase
+@interface reactnativewrTests : XCTestCase
 
 @end
 
-@implementation saspspTests
+@implementation reactnativewrTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
